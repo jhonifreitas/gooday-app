ab5c0a7c3c37f97122682ce09d5563d004ddc066
Jonathan Freitas <freitas.jonathan08@gmail.com>
2023-05-24 07:41:13 -0300

    notification

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,4 +1,7 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+
+import 'package:gooday/components/appbar.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
@@ -8,35 +11,116 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-  void _goToUser() {}
+  void _openGlycemiaForm() {}
+
+  void _goToMealForm() {
+    Navigator.pushNamed(context, '/calculadora/refeicao');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  bottom: 22, top: 10, left: 15, right: 20),
-              child: AppBar(
-                centerTitle: true,
-                backgroundColor: Colors.transparent,
-                leading: IconButton(
-                    onPressed: _goToUser,
-                    icon: const Icon(Icons.person_outline)),
-                title: const Text('Calculadora'),
-                actions: [
-                  IconButton(
-                      onPressed: _goToUser,
-                      icon: const Icon(Icons.edit_square)),
-                ],
-              ),
+      body: Column(
+        children: [
+          AppBarCustom(
+            prefix: const Icon(Icons.calculate_outlined),
+            title: const Text('Calculadora'),
+            suffix: SvgPicture.asset(width: 20, 'assets/icons/coin.svg'),
+          ),
+          SingleChildScrollView(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Material(
+                          clipBehavior: Clip.hardEdge,
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                          child: InkWell(
+                            onTap: _openGlycemiaForm,
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                children: const [
+                                  // SvgPicture.asset('assets/icons/blood.svg'),
+                                  Text(
+                                    'Calcular',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Refeição',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Material(
+                      clipBehavior: Clip.hardEdge,
+                      color: const Color(0xFF41BBD9),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: _openGlycemiaForm,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            children: const [
+                              // SvgPicture.asset('assets/icons/meal.svg'),
+                              Text(
+                                'Registrar',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                'Glicemia',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text('Hoje', style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+                Timeline(
+                  steps: const [
+                    Step(
+                      title: Text('Café da manhã'),
+                      content: Text('Ola'),
+                    )
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
